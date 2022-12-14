210f8d9a0419d2d396ddc7997f98bd4d5ebfcd36
thomas <tom.angotti11@gmail.com>
2022-12-14 09:48:01 -0700

    commit tree 00f4a7c2b5bbb76df8bf9593e5af44a494943c8c

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+    name: "Thomas Angotti",
+    streetAdress: "10911 W 16th street",
+}
+
+
+
+
+function updateEmployeeWithKeyAndValue(object, key, value){
+    const newEmployee = {...object};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value){
+    object[key] = value;
+    return object;
+}
+
+function deleteFromEmployeeByKey(object, key) {
+    const newEmployee = {...object};
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(object, key){
+    delete object[key]
+    return object;
+}
